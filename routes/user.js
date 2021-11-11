f0d7994f5ab4756409e697f894a5584befb8b465
Your Name <you@example.com>
2021-11-12 03:11:00 +0700

    6?

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
-const { userById, read, update } = require("../controllers/user");
 
-router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
-  res.json({ user: req.profile });
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory,
+} = require("../controllers/user");
+
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    user: "got here yay",
+  });
 });
 
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
 
 router.param("userId", userById);
 
